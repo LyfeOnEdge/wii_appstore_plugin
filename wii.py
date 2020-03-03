4f7fa14db92d5d12da34a0fda8b7b386191b76df
LyfeOnEdge <lyfeshomebrew@gmail.com>
2020-03-03 13:55:18 -0800

    Update wii.py

--- wii.py
+++ wii.py
@@ -1,6 +1,4 @@
-from gui.plugins import basePlugin
-from gui.widgets import categoryPage
-from gui.widgets import installedCategoryPage
+from gui.widgets import basePlugin, categoryPage, installedCategoryPage
 from appstore import Appstore
 
 REPO = "http://brewtools.dev/osc-redist/"
